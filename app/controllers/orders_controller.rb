979b756d789902cf1f96e06b40693df6d6ec8c2a
RiccardoK <webnoqs@gmail.com>
2017-10-02 00:58:17 +0200

    added colors to products

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+
+  def index
+    @orders = Order
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
