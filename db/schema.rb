0a420b2c4738cd40624a92e0d4e5af0e41f028fe
RiccardoK <webnoqs@gmail.com>
2017-10-09 04:49:48 +0200

    added lastname to orderstable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919201704) do
+ActiveRecord::Schema.define(version: 20170922024810) do
+
+  create_table "orders", force: :cascade do |t|
+    t.string "username"
+    t.string "product"
+    t.string "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
