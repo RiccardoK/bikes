109ae0a8806981ae93b182e6e02cce066c848cad
RiccardoK <webnoqs@gmail.com>
2017-10-11 03:45:27 +0200

    removed price from orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011011515) do
+ActiveRecord::Schema.define(version: 20171011014411) do
 
   create_table "orders", force: :cascade do |t|
     t.string "name"
     t.string "product"
-    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "lastname"
